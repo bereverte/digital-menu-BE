837a1a2fe28b00c7e9e3cd22bfeb577cf9d56fb8
Berta <bertareverte@gmail.com>
2024-12-05 15:48:54 +0100

    fix settings and delete pycache

--- digital_menu_backend/settings.py
+++ digital_menu_backend/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 
 DATABASES = {
-    "default": env.db("DATABASE_URL", default=f"sqlite:///{BASE_DIR}/db.sqlite3"),
+    "default": env.db("DATABASE_URL"),
 }
 
 
